b394fdcb5717f03b472bbf6ef7b3eedeee7515b7
Gaurav Joshi <codingmeanlogic008@gmail.com>
2026-01-15 22:27:07 +0545

    Login Page added. Oauth in process

--- Mail-Bud-Frontend/mailbud/lib/main.dart
+++ Mail-Bud-Frontend/mailbud/lib/main.dart
@@ -10,19 +10,48 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+      return MaterialApp(
       theme: ThemeData(
         brightness: Brightness.dark,
         primaryColor: Color.fromRGBO(10, 10, 10, 1),
         scaffoldBackgroundColor: Color.fromRGBO(10, 10, 10, 1)
       ),
-      title: 'Mail Bud',
+      title: 'Mail Bud - Login',
       debugShowCheckedModeBanner: false,
-      home: const MyHomePage(title: 'Mail Bud'),
+      home: const LoginPage(),
+      
     );
   }
 }
 
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+  final String google="""<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20" fill="#e3e3e3"><path d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"/><path d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z"/><path d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z"/><path d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z"/></svg>""";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(context, MaterialPageRoute(builder: (context) => MyHomePage(title: "Mail bud")));
+          },
+          // style: ElevatedButton.styleFrom(minimumSize: Size(80, 60)),
+          child: Padding(
+          padding: EdgeInsetsGeometry.only(top: 10, bottom: 10),
+          child: Row(mainAxisSize: MainAxisSize.min,
+           spacing: 20,
+          children: [SvgPicture.string(google),Text("Continue with Google", style: TextStyle(fontSize: 22),)
+          ]))
+        ),
+      ),
+    );
+  }
+}
+
+
+
+// Main Screen
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
   final String title;
@@ -183,6 +212,7 @@ class _MyHomePageState extends State<MyHomePage> {
     ][currentTabIndex]
     );
   }
+
 
 
   // Reusable Components
